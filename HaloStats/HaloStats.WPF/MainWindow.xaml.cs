c8645498c86e2b3fed32aab1de0ea8ff87a3c064
kmauzoul <58862511+kmauzoul@users.noreply.github.com>
2020-11-15 09:12:29 -0500

    Minor fixes
    Cleared the WPF application - will use as I follow tutorials and such
    
    Fixed references as well

--- HaloStats/HaloStats.WPF/MainWindow.xaml.cs
+++ HaloStats/HaloStats.WPF/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HaloStats.Library;
+using HaloStats.Http;
+using System.IO;
 
 namespace HaloStats.WPF
 {
@@ -23,6 +26,57 @@ namespace HaloStats.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private BitmapImage GetImageFromStream(Stream stream)
+        {
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.StreamSource = stream;
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.EndInit();
+            image.Freeze(); //This will allow me to use this image across threads
+            return image;
+        }
+
+        private async void BtnLoadGamertag_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //sloppy testing here but...
+                HttpREST rest = new HttpREST();
+                //Get Spartan and Emblem...
+                //Image spartanImage = Image.FromStream(await rest.GetImage($"profile/h5/profiles/{TxtGamertag.Text}/spartan?size=128"));
+                //Image emblemImage = Image.FromStream(await rest.GetImage($"profile/h5/profiles/{TxtGamertag.Text}/emblem?size=128"));
+
+                BitmapImage emblem = GetImageFromStream(await rest.GetImage($"profile/h5/profiles/{TxtGamertag.Text}/emblem?size=128"));
+                BitmapImage spartan = GetImageFromStream(await rest.GetImage($"profile/h5/profiles/{TxtGamertag.Text}/spartan?size=128"));
+
+                //Get player history
+                //var history = await rest.GetItem<PlayerMatchHistory>($"stats/h5/players/{TxtGamertag.Text}/matches?modes=Arena&start=0&count=10");
+
+
+                //StopProgress();
+                ImgEmblem.Source = emblem;
+                ImgSpartan.Source = spartan;
+            }
+            catch (Exception ex)
+            {
+                
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                //Cursor.Current = Cursors.Default;
+                //MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                //StopProgress();
+            }
+
+        }
+
+        private void TxtGamertag_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BtnLoadGamertag.IsEnabled = string.IsNullOrWhiteSpace(TxtGamertag.Text) ? false : true;
         }
     }
 }
